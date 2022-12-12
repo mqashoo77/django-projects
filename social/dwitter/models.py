c38d3969f3f5cbd8e18521c726d3159f1647af1b
mqashoo77 <mohammadqashoo77@gmail.com>
2022-12-12 05:02:29 +0200

    make the model for dtweet

--- social/dwitter/models.py
+++ social/dwitter/models.py
@@ -33,6 +33,12 @@ def create_profile(sender, instance, created, **kwargs):
         user_profile.follows.add(instance.profile)
         user_profile.save()
 
+class Dweet(models.Model):
+    user = models.ForeignKey(
+        User, related_name="dweets", on_delete=models.DO_NOTHING
+    )
+    body = models.CharField(max_length=140)
+    created_at = models.DateTimeField(auto_now_add=True)
 
 
     
