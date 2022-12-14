3c7c4741487cc3e7b66b45911412278505355c6e
mqashoo77 <mohammadqashoo77@gmail.com>
2022-12-14 05:56:22 +0200

    Update dashboard template and viwes

--- social/dwitter/views.py
+++ social/dwitter/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from .models import Profile
+from .forms import DweetForm
 
 def dashboard(request):
-    return render(request, "base.html")
-
+    if request.method == "POST":
+        form = DweetForm(request.POST)
+        if form.is_valid():
+            dweet = form.save(commit=False)
+            dweet.user = request.user
+            dweet.save()
+    form = DweetForm()
+    return render(request, "dwitter/dashboard.html", {"form": form})
+    
 def profile_list(request):
     profiles = Profile.objects.exclude(user=request.user)
     return render(request, "dwitter/profile_list.html", {"profiles": profiles})
